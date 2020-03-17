21ea43b8a90bdc759aaa8788b352216db7050d43
Vladislav Navoyan <nva112002@icloud.com>
2020-03-17 16:29:25 +0300

    Done task 1

---
+++ lesson1/task1.py
@@ -0,0 +1,7 @@
+a = 22
+number = 42
+number_user = int(input('Введите число: '))
+print(f'Ваше число - {number_user}, а заданное - {number}.')
+name = input('Введите своё имя: ')
+age = int(input('Введите свой возраст: '))
+print(f'Вас зовут {name}, и ваш возраст {age}')
