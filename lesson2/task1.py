55044b8fab2542dce953dfb75ac3ca716ee9eef9
Vladislav Navoyan <nva112002@icloud.com>
2020-03-21 20:20:56 +0300

    Done task 1

---
+++ lesson2/task1.py
@@ -0,0 +1,4 @@
+my_list = ['Строка', 123, 0.1, True, [1, 2, 3], ('Привет', 'Python'), {'this', 'is', 'not', 'dict'},
+           {'this': 'is', 'dict': 1}]
+for i in my_list:
+    print(f'{i} - это {type(i)}')
