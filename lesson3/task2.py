a75096f35460c3899f5f77ce89f1a15436f933a3
Vladislav Navoyan <nva112002@icloud.com>
2020-03-25 17:03:31 +0300

    Remark task 2

--- lesson3/task2.py
+++ lesson3/task2.py
@@ -2,4 +2,5 @@ def user(name, surname, year, city, email, phone):
     print(f'{name} {surname}, {year} year of birth, from {city}, email: {email}, phone: {phone}')
 
 
-user(input('name: '), input('surname: '), input('year: '), input('city: '), input('email: '), input('phone: '))
+user(name=input('name: '), surname=input('surname: '), year=input('year: '), city=input('city: '),
+     email=input('email: '), phone=input('phone: '))
