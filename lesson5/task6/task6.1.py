2ba60667e9805d796ef1ed2141c007ea882013da
Vladislav Navoyan <61428612+Bonzar@users.noreply.github.com>
2020-04-03 21:00:55 +0300

    The second way to solve task 6 (#7)
    P.S: retrieves numbers from a string
    P.S.S: modificated
    
    Co-authored-by: Vladislav Navoyan <nva112002@icloud.com>

--- lesson5/task6/task6.1.py
+++ lesson5/task6/task6.1.py
@@ -14,14 +14,13 @@ def numbs_out_str(line_with_numbers):
     for i in range(len(list_numbers_inline)):
         ind += 1
         try:
-            while line_with_numbers.find(list_numbers_inline[ind - 1]) - line_with_numbers.find(
-                    list_numbers_inline[ind], line_with_numbers.find(
-                            list_numbers_inline[ind - 1]) + 1) == -1:
+            while line_with_numbers.find(list_numbers_inline[ind - 1], line_with_numbers.find(
+                    numbers[0]) + count) - line_with_numbers.find(
+                list_numbers_inline[ind], line_with_numbers.find(
+                    numbers[0]) + count + 1) == -1:
                 numbers.append(list_numbers_inline[ind])
                 ind += 1
-                if line_with_numbers.find(list_numbers_inline[ind - 1]) == line_with_numbers.find(
-                        list_numbers_inline[ind]):
-                    count += 1
+                count += 1
             else:
                 full_number_list.append(int(reduce(lambda a, b: a + b, numbers)))
                 count = 0
@@ -34,7 +33,7 @@ def numbs_out_str(line_with_numbers):
 
 
 with open('text_6.txt', 'r', encoding='utf-8') as text_6:
-    dictSubject = {line.split(': ')[0].title(): line.split(': ')[1][:-1] for line in text_6.readlines()}
+    dictSubject = {line.split(': ')[0].title(): line.split(': ')[1] for line in text_6.readlines()}
     for key, value in dictSubject.items():
         dictSubject[key] = sum(numbs_out_str(value))
     print(dumps(dictSubject, indent=4, separators=('', ': ')))
